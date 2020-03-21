bb44a21ec4082c07040804ae838050a3119e943b
Dorian Volpe <vdoralex@gmail.com>
2020-03-21 16:17:18 +0100

    Ajout de ce j'ai codé avant de commncer le GIT

--- Test.js
+++
@@ -1,3 +0,0 @@
-//This is a test
-
-var hess = 1;
---
+++ server.js
@@ -0,0 +1,46 @@
+"use strict"
+
+
+/* Serveur pour le site de recettes */
+var express = require('express');
+var mustache = require('mustache-express');
+
+var app = express();
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+    secret: 'mot-de-passe-du-cookie',
+}));
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './views');
+
+/* Retourne la page principale */
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// retourne la mage d'inscription
+app.get('/inscriptiion', (req, res) => {
+    res.render('register');
+})
+
+
+// retourne la page profil
+app.get('/profil', (req, res) => {
+    res.render('profil');
+})
+app.post('/create', is_authenticated, (req, res) => {
+    var id = model.create(post_data_to_recipe(req));
+    res.redirect('/profil/' + id);
+});
+
+// page liste des jeux 
+app.get('/games', (req, res) => {
+    res.render('games');
+})
+app.post('/create', is_authenticated, (req, res) => {
+    var id = model.create(post_data_to_recipe(req));
+    res.redirect('/games/' + id);
+});
+
+app.listen(3000, () => console.log('listening on http://localhost:3000'));
