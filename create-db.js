a5007d30ef58a247853d7b20a908bc7c4b2bfa9b
Dorian Volpe <dorian@MacBook-Air-de-Dorian.local>
2020-03-25 17:03:37 +0100

    Save

--- create-db.js
+++ create-db.js
@@ -11,8 +11,8 @@ let db = new Sqlite3.Database(dbname, err => {
     console.log('Database stated on ' + dbname)
 
     //db.run('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)')
-    db.run('INSERT INTO user VALUES (@id, @name, @password)', [2, "zobi", "13"])
-    db.get('SELECT name FROM user WHERE id=2', (err, data) => {
+    //db.run('INSERT INTO user VALUES (@id, @name, @password)', [2, "zobi", "13"])
+    db.get('SELECT * FROM user WHERE id=2', (err, data) => {
         if (err)
             throw err;
         console.log(data);
