ea4d87303cefe8145cebdb1626149ab0a4b2fc9f
Dorian Volpe <vdoralex@gmail.com>
2020-04-02 14:37:04 +0200

    Readding Files after git failure

---
+++ controller/model.js
@@ -0,0 +1,21 @@
+"use strict"
+const Sqlite = require('../node_modules/better-sqlite3');
+
+let db = new Sqlite('db.sqlite');
+//connection
+exports.login = function(name, password) {
+
+    var user = db.prepare('SELECT id FROM user WHERE name=? AND password=?').get(name, password);
+    if (user.id != undefined) {
+        return user.id;
+    }
+    return -1;
+}
+
+//recuperer les infos d'un profil
+exports.printProfil = function(id) {
+
+    var taGrandMere = db.prepare('SELECT name FROM user WHERE id=?').get(id);
+    console.log(taGrandMere.name);
+    return taGrandMere.name;
+}
