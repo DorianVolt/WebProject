740d75e0748138b723904585bc7defe013385ba1
Dorian Volpe <vdoralex@gmail.com>
2020-04-03 13:15:37 +0200

    Deleting useless comments

--- controller/create-db.js
+++ controller/create-db.js
@@ -1,25 +1,4 @@
 "use strict"
-/*
-const fs = require('fs');
-const Sqlite3 = require('sqlite3');
-
-const dbname = 'main.db'
-
-let db = new Sqlite3.Database(dbname, err => {
-    if (err)
-        throw err
-    console.log('Database stated on ' + dbname)
-
-    //db.run('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)')
-    //db.run('INSERT INTO user(id, name, password) VALUES (?, ?, ?)', [2, "zobi", "13"])
-    db.get('SELECT name FROM user WHERE id=2', (err, data) => {
-        if (err)
-            throw err;
-        console.log(data);
-    })
-})*/
-
-const fs = require('fs');
 const Sqlite = require('better-sqlite3');
 
 let db = new Sqlite('db.sqlite');
