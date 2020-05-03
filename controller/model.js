625470ad213c4ecb8083114dcc22c6ab94bc5224
Dorian Volpe <vdoralex@gmail.com>
2020-05-03 05:09:43 +0200

    reformat des enfer

--- controller/model.js
+++ controller/model.js
@@ -1,154 +1,197 @@
-"use strict"
-const Sqlite = require('../node_modules/better-sqlite3');
+"use strict";
+const Sqlite = require("../node_modules/better-sqlite3");
 
-let db = new Sqlite('db.sqlite');
+let db = new Sqlite("db.sqlite");
 var fetch = require("node-fetch");
 
 //Connexion
 exports.login = function (name, password) {
-    var user = db.prepare('SELECT id FROM user WHERE name=? AND password=?').get(name, password);
-    if (user == undefined) {
-        return -1;
-    }
-    if (user.id != undefined) {
-        return user.id;
-    }
+  var user = db
+    .prepare("SELECT id FROM user WHERE name=? AND password=?")
+    .get(name, password);
+  if (user == undefined) {
     return -1;
-}
+  }
+  if (user.id != undefined) {
+    return user.id;
+  }
+  return -1;
+};
 
 //Inscription
 exports.register = function (name, password) {
-    db.prepare('INSERT INTO user VALUES (@id, @name, @password)').run({ id: null, name: name, password: password });
-}
+  db.prepare("INSERT INTO user VALUES (@id, @name, @password)").run({
+    id: null,
+    name: name,
+    password: password,
+  });
+};
 
 //Infos d'un profil
 exports.printProfil = function (id) {
-    var user = db.prepare('SELECT name FROM user WHERE id=?').get(id);
-    return user.name;
-}
+  var user = db.prepare("SELECT name FROM user WHERE id=?").get(id);
+  return user.name;
+};
 
 //Check MDP(pas identique ou trop court)
 exports.testMDP = function (MDP1, MDP2) {
-    if (MDP1 == "" || MDP2 == "" || MDP1 != MDP2 || MDP1.length < 6) {
-        return false;
-    } else return true;
-}
+  if (MDP1 == "" || MDP2 == "" || MDP1 != MDP2 || MDP1.length < 6) {
+    return false;
+  } else return true;
+};
 
 //Check identifiant
 exports.doubleName = function (name) {
-    var double;
-    double = db.prepare('SELECT id FROM user WHERE name=?').get(name);
-    return double == undefined
-}
+  var double;
+  double = db.prepare("SELECT id FROM user WHERE name=?").get(name);
+  return double == undefined;
+};
 
 //Requête à l'API
 exports.requestToApi = async function (page, gameName) {
-    var apiUrl = "https://api.rawg.io/api/games?page_size=10&search=" + gameName + "&page=" + page;
-    var repsonse = await fetch(apiUrl)
-    var json = await repsonse.json();
-    return json;
-}
+  var apiUrl =
+    "https://api.rawg.io/api/games?page_size=10&search=" +
+    gameName +
+    "&page=" +
+    page;
+  var repsonse = await fetch(apiUrl);
+  var json = await repsonse.json();
+  return json;
+};
 
 //Requête prescise à l'API
 exports.requestInfoToApi = async function (gameName) {
-    var apiUrl = "https://api.rawg.io/api/games/" + gameName;
-    var repsonse = await fetch(apiUrl)
-    var json = await repsonse.json();
-    return json;
-}
+  var apiUrl = "https://api.rawg.io/api/games/" + gameName;
+  var repsonse = await fetch(apiUrl);
+  var json = await repsonse.json();
+  return json;
+};
 
 //Ajout Photo
 exports.addPhoto = function (link, userId) {
-    //Supprime si jamais on veut la modifier et pas juste ajouter
-    db.prepare('DELETE from profilePictures WHERE userId=?').run(userId)
-    db.prepare('INSERT INTO profilePictures VALUES (@userId, @link)').run({ userId: userId, link: link });
-}
+  //Supprime si jamais on veut la modifier et pas juste ajouter
+  db.prepare("DELETE from profilePictures WHERE userId=?").run(userId);
+  db.prepare("INSERT INTO profilePictures VALUES (@userId, @link)").run({
+    userId: userId,
+    link: link,
+  });
+};
 
 //Recherche de photo
 exports.getImageById = function (userId) {
-    var image;
-    image = db.prepare('SELECT link FROM profilePictures WHERE userId=?').get(userId);
-    return image
-}
+  var image;
+  image = db
+    .prepare("SELECT link FROM profilePictures WHERE userId=?")
+    .get(userId);
+  return image;
+};
 
 //Ajoute un jeu à la liste d'un utilisateur
 exports.addGame = function (gameId, gameName, uid) {
-    var name = gameName
-    db.prepare('DELETE from game WHERE userId=? AND name=? AND id=?').run(uid, name, gameId)
-    db.prepare('INSERT INTO  game VALUES (@id,@name,@userId)').run({ id: gameId, name: name, userId: uid });
-}
+  var name = gameName;
+  db.prepare("DELETE from game WHERE userId=? AND name=? AND id=?").run(
+    uid,
+    name,
+    gameId
+  );
+  db.prepare("INSERT INTO  game VALUES (@id,@name,@userId)").run({
+    id: gameId,
+    name: name,
+    userId: uid,
+  });
+};
 
 //Retourne tout les jeux d'un utilisateur
 exports.getGamesById = function (userId) {
-    var games = db.prepare('Select name from game where userId=?').all(userId)
-    return games
-}
+  var games = db.prepare("Select name from game where userId=?").all(userId);
+  return games;
+};
 
 //Supprime un jeu de la liste de l'utilisateur
 exports.deleteGameById = function (gameName, userId) {
-    var game = gameName
-    db.prepare('DELETE from game WHERE userId=?AND name=?').run(userId, game);
-}
+  var game = gameName;
+  db.prepare("DELETE from game WHERE userId=?AND name=?").run(userId, game);
+};
 
 //Ajout un favoris
 exports.addFav = function (gameName, userId) {
-    var name = gameName
-    db.prepare('DELETE from favorites WHERE userId=? AND gameName=?').run(userId, name)
-    db.prepare('INSERT INTO  favorites VALUES (@userId,@name)').run({ userId: userId, name: name });
-}
+  var name = gameName;
+  db.prepare("DELETE from favorites WHERE userId=? AND gameName=?").run(
+    userId,
+    name
+  );
+  db.prepare("INSERT INTO  favorites VALUES (@userId,@name)").run({
+    userId: userId,
+    name: name,
+  });
+};
 
 //Supprime un favoris
 exports.deleteFav = function (gameName, userId) {
-    var game = gameName
-    db.prepare('DELETE from favorites WHERE userId=?AND gameName=?').run(userId, game);
-}
+  var game = gameName;
+  db.prepare("DELETE from favorites WHERE userId=?AND gameName=?").run(
+    userId,
+    game
+  );
+};
 
 //Retourne les favoris de l'utilisateur
 exports.getFavById = function (userId) {
-    var games = db.prepare('Select gameName from favorites where userId=?').all(userId)
-    return games
-}
+  var games = db
+    .prepare("Select gameName from favorites where userId=?")
+    .all(userId);
+  return games;
+};
 
 //Retourne un user selon son nom
 exports.getUserIdByName = function (name) {
-    var user = db.prepare('SELECT id FROM user WHERE name=?').get(name);
-    return user
-}
+  var user = db.prepare("SELECT id FROM user WHERE name=?").get(name);
+  return user;
+};
 
 //Retourne tout les utilisateurs du site
 exports.getUSERS = function () {
-    var users = db.prepare('SELECT id,name FROM user').all()
-    return users
-}
+  var users = db.prepare("SELECT id,name FROM user").all();
+  return users;
+};
 
 //Retourne les admins du site
 exports.getAdmins = function () {
-    var admins = db.prepare('SELECT id FROM admin').get()
-    return admins.id
-}
+  var admins = db.prepare("SELECT id FROM admin").get();
+  return admins.id;
+};
 
 //Suppression du compte
 exports.deleteProfile = function (userId) {
-    db.prepare('DELETE FROM user WHERE id=?').run(userId)
-    db.prepare('DELETE FROM game WHERE userId=?').run(userId)
-    db.prepare('DELETE FROM profilePictures WHERE userId=?').run(userId)
-    db.prepare('DELETE FROM favorites WHERE userId=?').run(userId)
-}
+  db.prepare("DELETE FROM user WHERE id=?").run(userId);
+  db.prepare("DELETE FROM game WHERE userId=?").run(userId);
+  db.prepare("DELETE FROM profilePictures WHERE userId=?").run(userId);
+  db.prepare("DELETE FROM favorites WHERE userId=?").run(userId);
+};
 
-//Trouve les utilisateurs ayant des jeux en commun avec la personne connectée 
+//Trouve les utilisateurs ayant des jeux en commun avec la personne connectée
 exports.getByAffinity = function (userId) {
-    var users = db.prepare('SELECT name FROM user WHERE id IN (SELECT userId FROM game WHERE name IN(SELECT name FROM game WHERE userId=?))GROUP BY id HAVING id<>?').all(userId,userId)
-    return users
-}
+  var users = db
+    .prepare(
+      "SELECT name FROM user WHERE id IN (SELECT userId FROM game WHERE name IN(SELECT name FROM game WHERE userId=?))GROUP BY id HAVING id<>?"
+    )
+    .all(userId, userId);
+  return users;
+};
 
 //Ajoute une description a la base de donnée
 exports.addDescription = function (desc, userId) {
-    db.prepare('DELETE from descriptions WHERE userId=?').run(userId)
-    db.prepare('INSERT INTO descriptions VALUES (@userId, @desc)').run({ userId: userId, desc: desc });
-}
+  db.prepare("DELETE from descriptions WHERE userId=?").run(userId);
+  db.prepare("INSERT INTO descriptions VALUES (@userId, @desc)").run({
+    userId: userId,
+    desc: desc,
+  });
+};
 
 //Retourne la description d'un utilisateur selon son id
-exports.getDescriptionById=function(userId){
-    var desc = db.prepare('SELECT desc FROM descriptions WHERE userId=?').get(userId)
-    return desc.desc
-}
+exports.getDescriptionById = function (userId) {
+  var desc = db
+    .prepare("SELECT desc FROM descriptions WHERE userId=?")
+    .get(userId);
+  return desc.desc;
+};
--- controller/server.js
+++ controller/server.js
@@ -1,18 +1,18 @@
-"use strict"
+"use strict";
 
-var mustache = require('../node_modules/mustache-express');
+var mustache = require("../node_modules/mustache-express");
 
-var model = require('./model');
+var model = require("./model");
 
-var express = require('../node_modules/express');
+var express = require("../node_modules/express");
 var app = express();
 
-var multer = require('../node_modules/multer')
-var upload = multer()
+var multer = require("../node_modules/multer");
+var upload = multer();
 
-var bodyParser = require('../node_modules/body-parser');
-var session = require('../node_modules/express-session');
-var cookieParser = require('../node_modules/cookie-parser');
+var bodyParser = require("../node_modules/body-parser");
+var session = require("../node_modules/express-session");
+var cookieParser = require("../node_modules/cookie-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,285 +20,301 @@ app.use(upload.array());
 app.use(cookieParser());
 app.use(session({ secret: "test", resave: false, saveUninitialized: true }));
 
-app.engine('html', mustache());
+app.engine("html", mustache());
 
-app.set('view engine', 'html');
-app.set('views', '../views');
+app.set("view engine", "html");
+app.set("views", "../views");
 
 //Inscription
-app.post('/register', (req, res) => {
-    var MDP = model.testMDP(req.body.password, req.body.passwordConfirm);
-    var username = model.doubleName(req.body.username);
-    if (!MDP || !username) {
-        res.redirect('/error');
-    } else {
-        model.register(req.body.username, req.body.password);
-        res.redirect('/validation');
-    }
-})
-
-//Page principale 
-app.post('/', (req, res) => {
-    if (req.session.authenticated == false) {
-        req.session.authenticated = false;
-        req.session.notauthenticated = true;
-        res.redirect('/');
-    } else res.redirect('/');
-})
-
-
-app.get('/', (req, res) => {
-    var session = req.session
-    res.render('index', session);
+app.post("/register", (req, res) => {
+  var MDP = model.testMDP(req.body.password, req.body.passwordConfirm);
+  var username = model.doubleName(req.body.username);
+  if (!MDP || !username) {
+    res.redirect("/error");
+  } else {
+    model.register(req.body.username, req.body.password);
+    res.redirect("/validation");
+  }
 });
 
-app.get('/error', (req, res) => {
-    res.render('error');
+//Page principale
+app.post("/", (req, res) => {
+  if (req.session.authenticated == false) {
+    req.session.authenticated = false;
+    req.session.notauthenticated = true;
+    res.redirect("/");
+  } else res.redirect("/");
 });
 
-app.get('/validation', (req, res) => {
-    res.render('validation');
+app.get("/", (req, res) => {
+  var session = req.session;
+  res.render("index", session);
 });
 
-app.get('/errorConnexion', (req, res) => {
-    res.render('errorConnexion');
+app.get("/error", (req, res) => {
+  res.render("error");
+});
+
+app.get("/validation", (req, res) => {
+  res.render("validation");
+});
+
+app.get("/errorConnexion", (req, res) => {
+  res.render("errorConnexion");
 });
 
 //Page d'inscription
-app.get('/register', (req, res) => {
-    res.render('register');
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 //Connexion
-app.post('/login', (req, res) => {
-    if (res.locals.authenticated == true) {
-        res.redirect('/profil');
-    } else {
-        var id = model.login(req.body.name, req.body.password);
-        req.session.pseudo = req.body.name;
-        req.session.user = id;
-        req.session.isAdmin = false
-        var adminId =model.getAdmins()
-        if(id == adminId){
-            req.session.isAdmin = true
-        }
-        res.redirect('/profil');
+app.post("/login", (req, res) => {
+  if (res.locals.authenticated == true) {
+    res.redirect("/profil");
+  } else {
+    var id = model.login(req.body.name, req.body.password);
+    req.session.pseudo = req.body.name;
+    req.session.user = id;
+    req.session.isAdmin = false;
+    var adminId = model.getAdmins();
+    if (id == adminId) {
+      req.session.isAdmin = true;
     }
-
-})
+    res.redirect("/profil");
+  }
+});
 
 //Page profil
-app.get('/profil', is_authenticated, (req, res) => {
-
-    var name = model.printProfil(req.session.user);
-    if (name == -1) {
-        console.log('Authentication failed !')
-        res.redirect('/error')
-    } else {
-        var games = model.getGamesById(req.session.user)
-        var authenticated = req.session.authenticated
-        var image = model.getImageById(req.session.user)
-        var desc = model.getDescriptionById(req.session.user)
-        var favorites = model.getFavById(req.session.user)
-        var hasGame = games.length != 0
-        var hasFav = favorites.length != 0
-        res.render('profil', { pseudo: name, games, authenticated, image, favorites, hasGame, hasFav,desc });
-    }
-})
-app.post('/profil', is_authenticated, (req, res) => {
-    res.redirect('/profil');
+app.get("/profil", is_authenticated, (req, res) => {
+  var name = model.printProfil(req.session.user);
+  if (name == -1) {
+    console.log("Authentication failed !");
+    res.redirect("/error");
+  } else {
+    var games = model.getGamesById(req.session.user);
+    var authenticated = req.session.authenticated;
+    var image = model.getImageById(req.session.user);
+    var desc = model.getDescriptionById(req.session.user);
+    var favorites = model.getFavById(req.session.user);
+    var hasGame = games.length != 0;
+    var hasFav = favorites.length != 0;
+    res.render("profil", {
+      pseudo: name,
+      games,
+      authenticated,
+      image,
+      favorites,
+      hasGame,
+      hasFav,
+      desc,
+    });
+  }
+});
+app.post("/profil", is_authenticated, (req, res) => {
+  res.redirect("/profil");
 });
 
 //Déconnexion
-app.get('/logout', is_authenticated, (req, res) => {
-    req.session.destroy()
-    res.redirect('/')
-})
+app.get("/logout", is_authenticated, (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
 
 //Middleware d'authentification
 function is_authenticated(req, res, next) {
-    if (req.session.user !== -1) {
-        req.session.authenticated = true;
-        req.session.notauthenticated = false;
-        return next();
-    }
-    res.redirect('/errorConnexion');
+  if (req.session.user !== -1) {
+    req.session.authenticated = true;
+    req.session.notauthenticated = false;
+    return next();
+  }
+  res.redirect("/errorConnexion");
 }
 
 //Page d'ajout de photo de profil
-app.get('/ajoutPhoto',  (req, res) => {
-    res.render('ajoutPhoto')
-})
+app.get("/ajoutPhoto", (req, res) => {
+  res.render("ajoutPhoto");
+});
 
-app.post('/ajoutPhoto',  (req, res) => {
-    model.addPhoto(req.body.lienImage, req.session.user)
-    res.redirect('/profil')
-})
+app.post("/ajoutPhoto", (req, res) => {
+  model.addPhoto(req.body.lienImage, req.session.user);
+  res.redirect("/profil");
+});
 
 //Suppression du compte
-app.get('/deleteAccount', is_authenticated, (req, res) => {
-    res.render('deleteAccount')
-})
+app.get("/deleteAccount", is_authenticated, (req, res) => {
+  res.render("deleteAccount");
+});
 
-app.post('/deleteAccount',is_authenticated,  (req, res) => {
-    model.deleteProfile(req.session.user)
-    req.session.destroy()
-    res.redirect('/')
-})
+app.post("/deleteAccount", is_authenticated, (req, res) => {
+  model.deleteProfile(req.session.user);
+  req.session.destroy();
+  res.redirect("/");
+});
 
 //Page d'ajout d'une biographie'
-app.get('/ajoutDescription',  (req, res) => {
-    res.render('ajoutDescription')
-})
+app.get("/ajoutDescription", (req, res) => {
+  res.render("ajoutDescription");
+});
 
-app.post('/ajoutDescription',  (req, res) => {
-    model.addDescription(req.body.description,req.session.user )
-    res.redirect('/profil')
-})
+app.post("/ajoutDescription", (req, res) => {
+  model.addDescription(req.body.description, req.session.user);
+  res.redirect("/profil");
+});
 
 //Bannissement d'un compte
-app.post('/ban/:id', is_authenticated, (req, res) => {
-    var id = req.params.id
-    model.deleteProfile(id)
-    res.redirect('/users')
-})
+app.post("/ban/:id", is_authenticated, (req, res) => {
+  var id = req.params.id;
+  model.deleteProfile(id);
+  res.redirect("/users");
+});
 
 //Requête à l'API ------------------------------------------------------------------------------------------------------------------------------------------
 
 var page;
 var resultat;
 var hasNext = true;
-var hasPrev = false
+var hasPrev = false;
 
 //Page de jeu (recherche)
-app.post('/game', async (req, res) => {
-    page = 1
-    resultat = req.body.research;
-    req.params.research = resultat
-    var result = await model.requestToApi(page, resultat)
-    if (result.count == 0) {
-        res.redirect('/')
+app.post("/game", async (req, res) => {
+  page = 1;
+  resultat = req.body.research;
+  req.params.research = resultat;
+  var result = await model.requestToApi(page, resultat);
+  if (result.count == 0) {
+    res.redirect("/");
+  } else {
+    result.authenticated = req.session.authenticated;
+    result.hasNext = hasNext;
+    result.hasPrev = hasPrev;
+    if (req.session.authenticated) {
+      result.pseudo = model.printProfil(req.session.user);
     }
-    else {
-        result.authenticated = req.session.authenticated
-        result.hasNext = hasNext
-        result.hasPrev = hasPrev
-        if (req.session.authenticated) { result.pseudo = model.printProfil(req.session.user); }
-        res.render('game', result)
-    }
-})
+    res.render("game", result);
+  }
+});
 
-//Page de jeu 
-app.get('/game', async (req, res) => {
-    var result = await model.requestToApi(page, resultat)
-    if (result.count == 0) {
-        res.redirect('/')
+//Page de jeu
+app.get("/game", async (req, res) => {
+  var result = await model.requestToApi(page, resultat);
+  if (result.count == 0) {
+    res.redirect("/");
+  } else {
+    //Navigation des pages
+    if (page == 1) {
+      hasPrev = false;
     }
-    else {
-        //Navigation des pages
-        if (page == 1) {
-            hasPrev = false
-        }
-        if (page != 1) {
-            hasPrev = true
-        }
-        if (result.count <= page * 10) {
-            hasNext = false
-        }
-        if (page * 10 < result.count) {
-            hasNext = true
-        }
-        result.authenticated = req.session.authenticated
-        result.hasNext = hasNext
-        result.hasPrev = hasPrev
-        //Transmet le pseudo au render uniquement si l'utilisateur est connecté
-        if (req.session.authenticated) { result.pseudo = model.printProfil(req.session.user); }
-        res.render('game', result)
+    if (page != 1) {
+      hasPrev = true;
     }
-})
+    if (result.count <= page * 10) {
+      hasNext = false;
+    }
+    if (page * 10 < result.count) {
+      hasNext = true;
+    }
+    result.authenticated = req.session.authenticated;
+    result.hasNext = hasNext;
+    result.hasPrev = hasPrev;
+    //Transmet le pseudo au render uniquement si l'utilisateur est connecté
+    if (req.session.authenticated) {
+      result.pseudo = model.printProfil(req.session.user);
+    }
+    res.render("game", result);
+  }
+});
 
 //Prochaine page
-app.get('/next', async (req, res) => {
-    page++
-    res.redirect('/game')
-})
+app.get("/next", async (req, res) => {
+  page++;
+  res.redirect("/game");
+});
 
 //Page précedente
-app.get('/previous', async (req, res) => {
-    page--
-    res.redirect('/game')
-})
+app.get("/previous", async (req, res) => {
+  page--;
+  res.redirect("/game");
+});
 
 //Ajout d'un jeu dans la liste de l'utilisateur
-app.post('/ajout/:id/:name', is_authenticated, (req, res) => {
-    model.addGame(req.params.id, req.params.name, req.session.user)
-})
+app.post("/ajout/:id/:name", is_authenticated, (req, res) => {
+  model.addGame(req.params.id, req.params.name, req.session.user);
+});
 
 //Delete un jeu de la liste
-app.post('/delete/:name', is_authenticated, (req, res) => {
-    model.deleteGameById(req.params.name, req.session.user)
-    res.redirect('/profil')
-})
+app.post("/delete/:name", is_authenticated, (req, res) => {
+  model.deleteGameById(req.params.name, req.session.user);
+  res.redirect("/profil");
+});
 
 //Ajout d'un jeu dans la liste des favoris de l'utilisateur
-app.post('/ajoutFav/:name', is_authenticated, (req, res) => {
-    model.addFav(req.params.name, req.session.user)
-    res.redirect('/profil')
-})
+app.post("/ajoutFav/:name", is_authenticated, (req, res) => {
+  model.addFav(req.params.name, req.session.user);
+  res.redirect("/profil");
+});
 
 //Delete un jeu de la liste
-app.post('/deleteFav/:name', is_authenticated, (req, res) => {
-    model.deleteFav(req.params.name, req.session.user)
-    res.redirect('/profil')
-})
+app.post("/deleteFav/:name", is_authenticated, (req, res) => {
+  model.deleteFav(req.params.name, req.session.user);
+  res.redirect("/profil");
+});
 
 //Page profil via recherche
-app.post('/userSearch', (req, res) => {
-    var name = req.body.searchName
-    var idResearch = model.getUserIdByName(name)
-    if (idResearch == undefined) {
-        res.render('noSuchUser')
-    }
-    else {
-        var userId = idResearch.id
-        var games = model.getGamesById(userId)
-        var image = model.getImageById(userId)
-        var favorites = model.getFavById(userId)
-        var desc = model.getDescriptionById(req.session.user)
-        var hasGame = games.length != 0
-        var hasFav = favorites.length != 0
-        var authenticated = req.session.authenticated
-        res.render('profilSearch', { pseudo: name, games, image, favorites, hasGame, hasFav ,authenticated,desc});
-    }
-})
+app.post("/userSearch", (req, res) => {
+  var name = req.body.searchName;
+  var idResearch = model.getUserIdByName(name);
+  if (idResearch == undefined) {
+    res.render("noSuchUser");
+  } else {
+    var userId = idResearch.id;
+    var games = model.getGamesById(userId);
+    var image = model.getImageById(userId);
+    var favorites = model.getFavById(userId);
+    var desc = model.getDescriptionById(req.session.user);
+    var hasGame = games.length != 0;
+    var hasFav = favorites.length != 0;
+    var authenticated = req.session.authenticated;
+    res.render("profilSearch", {
+      pseudo: name,
+      games,
+      image,
+      favorites,
+      hasGame,
+      hasFav,
+      authenticated,
+      desc,
+    });
+  }
+});
 
 //Retourne la liste de sutilisateur pour une recherche plus simple
-app.get('/users', (req,res)=> {
-    var users = model.getUSERS()
-    var authenticated = req.session.authenticated
-    var sorted = false;
-    var isAdmin = req.session.isAdmin
-    res.render('users', {users,authenticated,sorted,isAdmin})
-})
+app.get("/users", (req, res) => {
+  var users = model.getUSERS();
+  var authenticated = req.session.authenticated;
+  var sorted = false;
+  var isAdmin = req.session.isAdmin;
+  res.render("users", { users, authenticated, sorted, isAdmin });
+});
 
 //Trie les utilisateurs par affinité par rapport a l'utilisateur connecté
-app.post('/sortByAffinity',(req,res) =>{
-    var users = model.getByAffinity(req.session.user)
-    var authenticated = req.session.authenticated
-    var sorted = true;
-    res.render('users', {users,authenticated,sorted})
-})
+app.post("/sortByAffinity", (req, res) => {
+  var users = model.getByAffinity(req.session.user);
+  var authenticated = req.session.authenticated;
+  var sorted = true;
+  res.render("users", { users, authenticated, sorted });
+});
 
+app.get("/gameInfo/:slug", async (req, res) => {
+  var gameName = req.params.slug;
+  var result = await model.requestInfoToApi(gameName);
+  result.authenticated = req.session.authenticated;
+  if (req.session.authenticated) {
+    result.pseudo = model.printProfil(req.session.user);
+  }
+  res.render("gameInfo", result);
+});
 
-app.get('/gameInfo/:slug', async (req, res) => {
-    var gameName = req.params.slug 
-    var result = await model.requestInfoToApi(gameName)
-        result.authenticated = req.session.authenticated
-        if (req.session.authenticated) { result.pseudo = model.printProfil(req.session.user); }
-        res.render('gameInfo', result)
-
-})
-
-
-
-app.listen(8000, () => console.log('The server is running at http://localhost:8000'));
+app.listen(8000, () =>
+  console.log("The server is running at http://localhost:8000")
+);
